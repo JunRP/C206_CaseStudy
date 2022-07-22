7ddfccded03d2d1ce052a7044fb0606e675eb70b
21038177 <21038177@LAPTOP-8VBOGQ9T>
2022-07-22 23:15:07 +0800

    3rd Push

--- C206_CaseStudy/src/Package.java
+++ C206_CaseStudy/src/Package.java
@@ -1,4 +1,46 @@
-
+import java.time.LocalDate;
 public class Package {
+	private String Code;
+	private String Description;
+	private LocalDate Start;
+	private LocalDate End;
+	private double Amount;
+	
+	public Package(String code, String description, LocalDate start, LocalDate end, double amount) 
+	{
+		Code = code;
+		Description = description;
+		Start = start;
+		End = end;
+		Amount = amount;
+	}
+
+	public void setCode(String code) 
+	{
+		Code = code;
+	}
+
+	public void setDescription(String description) 
+	{
+		Description = description;
+	}
+
+	public void setStart(LocalDate start) 
+	{
+		Start = start;
+	}
+
+	public void setEnd(LocalDate end) 
+	{
+		End = end;
+	}
+
+	public void setAmount(double amount) 
+	{
+		Amount = amount;
+	}
+	
+	
+	
 
 }
