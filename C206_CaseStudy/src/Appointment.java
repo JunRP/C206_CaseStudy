56fe8dad7019e56b0b63c8e9445ce3da709de659
21040827 <21040827@10.174.123.98>
2022-07-25 16:07:03 +0800

    3rd Appointment Push

--- C206_CaseStudy/src/Appointment.java
+++ C206_CaseStudy/src/Appointment.java
@@ -50,4 +50,4 @@ public class Appointment {
 	
 	
 	
-}// end
+}// end of appointment
--- C206_CaseStudy/src/Package.java
+++ C206_CaseStudy/src/Package.java
@@ -1,4 +1,13 @@
 
 public class Package {
+	
+	private String id;
+	private String description;
+	private String type;
+	private double amount;
+	private String pStartDate;
+	private String pEndDate;
+	
+	
 
 }
